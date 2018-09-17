6189957bc670bc91c54f70328f515af9586b3043
standon19 <42978500+standon19@users.noreply.github.com>
2018-09-17 14:31:04 -0400

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+public int add(int num1, int num2)
+
+int ans = num1 + num2;
+return ans;
+	}
+
+}
---
+++ CalculatorMain.java
@@ -0,0 +1,11 @@
+
+public class CalculatorMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Calculator calc = new Calculator();
+	System.out.print(calc.add(2, 4));
+	}
+
+}
